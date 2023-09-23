23826b139a50db37b474efeb7343f1c0a35c8932
nikanzeyaei <n.ziyaee@tasu.ventures>
2023-09-23 13:59:18 +0330

    feat: add gopls to LSPs

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -46,6 +46,7 @@ end
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
+  gopls = {},
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
