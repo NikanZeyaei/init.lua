7444a0c6025a174ac0aba5f35766eb50f9fd1496
nikanzeyaei <n.ziyaee@tasu.ventures>
2023-09-23 15:08:08 +0330

    add some other LSPs

--- lua/nikanzeyaei/lsp.lua
+++ lua/nikanzeyaei/lsp.lua
@@ -46,7 +46,12 @@ end
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
+  denols = {},
+  tsserver = {},
   gopls = {},
+  pylsp = {},
+  jsonls = {},
+  yamlls = {},
   lua_ls = {
     Lua = {
       workspace = { checkThirdParty = false },
