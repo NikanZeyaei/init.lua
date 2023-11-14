400061a4a2188961d4d3967cb06ca29aa12ae6a0
nikanzeyaei <nikanzeyaei@gmail.com>
2023-11-14 12:21:47 +0330

    feat: finally fixed the transparency problem

--- lua/nikanzeyaei/sets.lua
+++ lua/nikanzeyaei/sets.lua
@@ -32,6 +32,5 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "Term", { bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalNC", { link = "Normal" })
