25ea16bd8573362d0867fd2148db9f46d48153d6
nikanzeyaei <nikanzeyaei@gmail.com>
2023-11-19 16:40:24 +0330

    feat: remove comment

--- lua/nikanzeyaei/lazy.lua
+++ lua/nikanzeyaei/lazy.lua
@@ -184,20 +184,6 @@ require('lazy').setup({
         end,
       },
     },
-    -- config = function()
-    --   require("telescope").setup({
-    --     extensions = {
-    --       undo = {
-    --         side_by_side = true,
-    --         layout_strategy = "vertical",
-    --         layout_config = {
-    --           preview_height = 0.8,
-    --         },
-    --       },
-    --     },
-    --   })
-    --   require("telescope").load_extension("undo")
-    -- end,
   },
 
   {
