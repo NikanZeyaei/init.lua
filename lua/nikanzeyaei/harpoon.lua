fe5d4c150d0b182bf57f11645e81c18f9208d771
nikanzeyaei <nikanzeyaei@gmail.com>
2023-11-20 16:19:22 +0330

    feat: these keybindings are messing up my <C-w>. Have to find alternatives

--- lua/nikanzeyaei/harpoon.lua
+++ lua/nikanzeyaei/harpoon.lua
@@ -4,5 +4,5 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon: Add current file" })
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Harpoon: Toggle quick menu" })
 
-vim.keymap.set("n", "<C-q>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-w>", function() ui.nav_file(2) end)
+-- vim.keymap.set("n", "<C-q>", function() ui.nav_file(1) end)
+-- vim.keymap.set("n", "<C-w>", function() ui.nav_file(2) end)
