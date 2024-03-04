ef97d6742247a85016e37b46990be5916e1ec803
Nikan Zeyaei <nikanzeyaei@gmail.com>
2024-03-04 14:31:44 +0330

    feat(lazy): remove which-key

--- lua/nikanzeyaei/lazy.lua
+++ lua/nikanzeyaei/lazy.lua
@@ -230,8 +230,6 @@ require('lazy').setup({
     },
   },
 
-  -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',  opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
