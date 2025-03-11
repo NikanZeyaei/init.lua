dc9f0669a79632a4bdd370ed050367873e0107cb
Nikan Zeyaei <you@example.com>
2025-03-11 14:06:42 +0330

    feat: remove cspell

--- lua/nikanzeyaei/lsp.lua
+++ lua/nikanzeyaei/lsp.lua
@@ -37,22 +37,6 @@ local on_attach = function(client, bufnr)
   end, { desc = 'Format current buffer with LSP' })
 end
 
-require('lint').linters_by_ft = {
-  typescript = { 'cspell' }
-}
-
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  callback = function()
-    -- try_lint without arguments runs the linters defined in `linters_by_ft`
-    -- for the current filetype
-    require("lint").try_lint()
-
-    -- You can call `try_lint` with a linter name or a list of names to always
-    -- run specific linters, independent of the `linters_by_ft` configuration
-    require("lint").try_lint("cspell")
-  end,
-})
-
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
 --
