46dc5a2b0d67441fb39dae39ed8a9d82eecddebe
Nikan Zeyaei <you@example.com>
2025-03-26 00:00:10 +0330

    fix(fugitive): pull

--- lua/nikanzeyaei/plugins/fugitive.lua
+++ lua/nikanzeyaei/plugins/fugitive.lua
@@ -20,7 +20,7 @@ return {
 
                 -- rebase always
                 vim.keymap.set("n", "<leader>P", function()
-                    vim.cmd.Git({"pull",  "--rebase"})
+                    vim.cmd.Git("pull --rebase")
                 end, {buffer = bufnr, remap = false, desc = "Git Pull Rebase"})
 
                 -- It allows me to easily set the branch i am pushing and any tracking
